775287858c256b7147386685526d2259a07b70c2
ahmedashmil22 <ahmedashmil022@gmail.com>
2026-04-13 18:16:40 +0530

    Update generate_sinusoid.m

--- generate_sinusoid.m
+++ generate_sinusoid.m
@@ -1,16 +1,19 @@
-function [outputArg1,outputArg2] = generate_sinusoid(inputArg1,inputArg2)
-%GENERATE_SINUSOID Summary of this function goes here
-%   Detailed explanation goes here
-arguments (Input)
-    inputArg1
-    inputArg2
-end
+function [t, x] = generate_sinusoid(f, A)
+%GENERATE_SINUSOID Generate a sinusoidal signal
+% Inputs:
+%   f        - signal frequency (Hz)
+%   A        - amplitude
+%
+% Outputs:
+%   t - time vector
+%   x - sinusoidal signal
 
-arguments (Output)
-    outputArg1
-    outputArg2
-end
+fs = 8000;        % sampling frequency
+duration = 1;     % sec
 
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+t = 0:1/fs:duration-1/fs;
+
+% sinusoid fn
+x = A*sin(2*pi*f*t);
+
 end
