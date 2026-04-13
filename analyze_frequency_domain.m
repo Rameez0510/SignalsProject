7a63c15896ad8849fb670651e5468e0e63715143
Rameez0510 <khanrameezrk428@gmail.com>
2026-04-13 15:02:43 +0530

    files created

---
+++ analyze_frequency_domain.m
@@ -0,0 +1,16 @@
+function [outputArg1,outputArg2] = analyze_frequency_domain(inputArg1,inputArg2)
+%ANALYZE_FREQUENCY_DOMAIN Summary of this function goes here
+%   Detailed explanation goes here
+arguments (Input)
+    inputArg1
+    inputArg2
+end
+
+arguments (Output)
+    outputArg1
+    outputArg2
+end
+
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
